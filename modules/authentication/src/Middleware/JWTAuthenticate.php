10a60351bf57095ed2a40c65218a9dc4d3154d82
Esteban <esteban.gobert@me.com>
2020-10-27 19:29:49 +0100

    handle exception token expired

--- modules/authentication/src/Middleware/JWTAuthenticate.php
+++ modules/authentication/src/Middleware/JWTAuthenticate.php
@@ -10,6 +10,7 @@ use Illuminate\Session\TokenMismatchException;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Token;
 
@@ -24,19 +25,34 @@ class JWTAuthenticate
      */
     public function handle(Request $request, Closure $next)
     {
+        // caching the next action
+        $response = $next($request);
+
         try {
-            if(!$request->cookies->has('XSRF-TOKEN')) throw new TokenMismatchException();
+            if (!$request->cookies->has('XSRF-TOKEN')) throw new TokenMismatchException();
 //            $rawToken = $_COOKIE['access_token'];
             $rawToken = $request->cookie('access_token');
             $token = new Token($rawToken);
             $payload = JWTAuth::decode($token);
             $user = User::findOrfail($payload->get('sub'));
             auth()->login($user);
-        } catch(Exception $e) {
-            if($e instanceof TokenExpiredException) {
-                // TODO token refresh here
+        } catch (Exception $e) {
+            if ($e instanceof TokenExpiredException) {
+                // If the token is expired, then it will be refreshed and added to the headers
+                try {
+                    $refreshed = JWTAuth::refresh(JWTAuth::getToken());
+                    $user = JWTAuth::setToken($refreshed)->toUser();
+                    header('Authorization: Bearer ' . $refreshed);
+                    auth()->login($user);
+                } catch (JWTException $e) {
+                    return response()->json([
+                        'code' => 103, // means not refreshable
+                        'response' => null // nothing to show
+                    ]);
+                }
+            } else {
+                return response('Unauthorized', 401);
             }
-            return response('Unauthorized', 401);
         }
 
         return $next($request);

5bf9bd904b22c41cc28d65b1f21c62aa6aebaea3
Baiquette <besseyre.cyril@gmail.com>
2020-11-17 17:43:03 +0100

    Force HTTPS

---
+++ apps/authentication/app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
+            \URL::forceScheme('https');
+        }
+        return $next($request);
+    }
+}
